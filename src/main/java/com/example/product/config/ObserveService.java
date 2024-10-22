a81ea90f0e933336144039a9df2c1b3c911d21df
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2024-10-22 16:46:39 -0400

    Please enter the commt message for your changes. Lines starting

--- src/main/java/com/example/product/config/ObserveService.java
+++ src/main/java/com/example/product/config/ObserveService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
-public class git add .ObserveService {
+public class ObserveService {
     private final Random random = new Random();
     private static final Logger log = LoggerFactory.getLogger(ObserveService.class);
 
